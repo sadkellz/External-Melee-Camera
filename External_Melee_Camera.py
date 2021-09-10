d085aeb4a2fe7b97187d1ed15f190428c3217718
KELLZ <forestruns@gmail.com>
2021-09-10 20:01:00 -0300

    Screenshot Sequence WIP

--- External_Melee_Camera.py
+++ External_Melee_Camera.py
@@ -1,10 +1,9 @@
 import bpy
+import time
 import struct
 from ctypes import *
 from ctypes.wintypes import *
 import psutil
-from pywinauto import Application, keyboard
-
 from bpy.props import (StringProperty,
                        BoolProperty,
                        IntProperty,
@@ -18,6 +17,7 @@ from bpy.types import (Panel,
                        Operator,
                        PropertyGroup,
                        )
+from pywinauto import Application, keyboard
 
 # ------------------------------------------------------------------------
 #    Scene Properties
@@ -62,7 +62,7 @@ def process_ops(pid, addr, buf, bufSize, bytesWritten):
     
     CloseHandle(processHandle)
 
-'''def dol_pid():
+def dol_pid():
     # psutil to automatically grab Dolphins PID.
     process_name = "Slippi Dolphin"
     pid = None
@@ -71,10 +71,15 @@ def process_ops(pid, addr, buf, bufSize, bytesWritten):
         if process_name in proc.name():
             pid = proc.pid
     return pid
-    # how tf does one implement this
-'''
-def sync_blender_cam():
-    pid = int(bpy.context.scene.emc_tool.pid_entry)
+
+def util_dol():
+    pid = dol_pid()
+    app = Application().connect(process=pid)
+    win = app['Faster Melee - Slippi(2.3.6) - Playback']
+    # Changes focus and sends key.
+    return win
+
+def sync_blender_cam(pid):
 
         # Define Camera and Origin
     org = bpy.data.objects['Origin']
@@ -104,8 +109,8 @@ def sync_blender_cam():
     bytesWritten = c_ulonglong()
     process_ops(pid, addr, buf, bufSize, bytesWritten)
 
-def sync_player_control():
-    pid = int(bpy.context.scene.emc_tool.pid_entry)
+def sync_player_control(pid):
+
         # Check if animation is playing.
     if bpy.context.screen.is_animation_playing:
         anim_byte = 0
@@ -127,13 +132,15 @@ class Sync_Cam_Pos(bpy.types.Operator):
     bl_idname = "wm.sync_cam"
     bl_label = "Sync Camera"
     _timer = None
+    _pid = dol_pid()
+
     def modal(self, context, event):
         if event.type in {'Q'}:
             self.cancel(context)
             return {'CANCELLED'}
 
         if event.type == 'TIMER':
-            sync_blender_cam()
+            sync_blender_cam(self._pid)
         return {'PASS_THROUGH'}
 
     def execute(self, context):
@@ -151,14 +158,15 @@ class Sync_Cam_Pos(bpy.types.Operator):
 class Sync_Media_Controls(bpy.types.Operator):
     bl_idname = "sync.mc"
     bl_label = "Sync Media Controls"
-
+    _timer = None
+    _pid = dol_pid()
     def modal(self, context, event):
         if not context.window_manager.sync_m_toggle:
             context.window_manager.event_timer_remove(self._timer)
             return {'FINISHED'}
 
         if event.type == 'TIMER':
-            sync_player_control()
+            sync_player_control(self._pid)
         return {'PASS_THROUGH'}
 
     def invoke(self, context, event):
@@ -172,18 +180,58 @@ class Sync_Media_Controls(bpy.types.Operator):
         wm = context.window_manager
         wm.event_timer_remove(self._timer)
 
-class Load_Save_State(bpy.types.Operator):
-    bl_idname = "wm.load_ss"
-    bl_label = "Load Save State"
+class Screenshot_Sequence(bpy.types.Operator):
+    """test"""
+    bl_idname = "wm.ss_seq"
+    bl_label = "Image Sequence"
+    _timer = None
+    ss_frame = -1
+    #_win = util_dol()
+    wait = 2
+
+    def modal(self, context, event):
+        scene = context.scene
+        seq_active = self.ss_frame == scene.frame_current
+
+        if event.type in {'ESC'} or scene.frame_current > scene.frame_end:
+            self.cancel(context)
+            return {'CANCELLED'}
+
+        if event.type == 'TIMER' and self.wait:
+            self.wait -= 1
+            return {'PASS_THROUGH'}
+
+        if seq_active:
+            return {'PASS_THROUGH'}
+            
+        else:
+            self.ss_frame = scene.frame_current
+            #self._win.set_focus()
+            print("taking screenshot")
+            #keyboard.send_keys("{VK_F4 down}"
+            #                    "{VK_F4 up}")
+            print("advance frame")
+            bpy.ops.screen.frame_offset(delta=1)
+            self.wait = 1
+        return {'PASS_THROUGH'}
 
     def execute(self, context):
-        sync_frame_end()
-        return {'FINISHED'}
+        scene = context.scene
+        wm = context.window_manager
+        self._timer = wm.event_timer_add(1, window=context.window)
+        wm.modal_handler_add(self)
+        scene.frame_set(scene.frame_start)
+        print("ahhhh")
+        return {'RUNNING_MODAL'}
+
+    def cancel(self, context):
+        wm = context.window_manager
+        wm.event_timer_remove(self._timer)
 
 class Multi_Op(bpy.types.Operator):
     bl_idname = "multi.op"
     bl_label = "Multiple Operators"
-
+    
     action: EnumProperty(
     items=[
         ('LOAD', 'load state', 'load state'),
@@ -193,20 +241,33 @@ class Multi_Op(bpy.types.Operator):
     def execute(self, context):
         if self.action == 'LOAD':
             self.sync_frame_end()
+        '''elif self.action == 'SEQUENCE':
+            self.screen_seq()'''
         return {'FINISHED'}
 
     @staticmethod
     def sync_frame_end():
+        _win = util_dol()
         # Go to frame start.
         bpy.ops.screen.frame_jump(end=False)
-        pid = int(bpy.context.scene.emc_tool.pid_entry)
-        app = Application().connect(process=pid)
-        win = app['Faster Melee - Slippi(2.3.6) - Playback']
         # Changes focus and sends key.
-        win.set_focus()
+        _win.set_focus()
         keyboard.send_keys("{VK_F3 down}"
                             "{VK_F3 up}")
     
+    '''@staticmethod
+    def screen_seq():
+        _win = util_dol()
+        frames = bpy.context.scene.frame_end
+        bpy.ops.screen.frame_jump(end=False)
+        
+    def screen
+        while range(frames):
+            _win.set_focus()
+            keyboard.send_keys("{VK_F4 down}"
+                                "{VK_F4 up}")
+            time.sleep(1)
+            bpy.ops.screen.frame_offset(delta=1)'''
 
 # ------------------------------------------------------------------------
 #    Panel in Object Mode
@@ -232,10 +293,12 @@ class EMC_Control_Panel(Panel):
         EMCTool = scene.emc_tool
         label_sync = "Sync Media Controls" if wm.sync_m_toggle else "Sync Media Controls"
         
-        layout.prop(EMCTool, "pid_entry")
+        #layout.prop(EMCTool, "pid_entry")
         layout.operator("wm.sync_cam")
         layout.prop(wm, 'sync_m_toggle', text=label_sync, toggle=True)
-        layout.operator('multi.op', text='Load Save State').action = 'LOAD'
+        #layout.operator('multi.op', text='Load Save State').action = 'LOAD'
+        layout.operator("wm.ss_seq")
+        #layout.operator('multi.op', text='Screenshot Sequence').action = 'SEQUENCE'
         layout.separator()
 
     def update_function(self, context):
@@ -256,6 +319,7 @@ classes = (
     Sync_Cam_Pos,
     Sync_Media_Controls,
     Multi_Op,
+    Screenshot_Sequence,
 
 )
 
