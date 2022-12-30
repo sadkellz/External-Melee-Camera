ae063bfd7021e319d1a87a23205807fcad92797b
KELLZ <forestruns@gmail.com>
2022-12-30 06:30:22 -0330

    Added operators for new save/load state from slot.

--- External-Melee-Camera/emc_op.py
+++ External-Melee-Camera/emc_op.py
@@ -4,10 +4,11 @@ import os
 from pathlib import Path
 from .emc_common import GALE01, CAM_START, PAUSE_BIT, CAM_TYPE
 from .emc_functions import sync_blender_cam, sync_player_control,\
-    take_screenshot, frame_step, load_state, save_state, set_player_pos
+    take_screenshot, frame_step, load_state, save_state, set_player_pos, \
+    save_slot_state, load_slot_state
 
 
-class syncCamera(bpy.types.Operator):
+class menu_sync_camera(bpy.types.Operator):
     """A timer that consistently writes to Dolphins memory"""
     bl_idname = "wm.sync_cam"
     bl_label = "Sync Camera"
@@ -63,7 +64,7 @@ def prev_seq(context):
     bpy.ops.screen.frame_offset(delta=1)
 
 
-class screenshotSequence(bpy.types.Operator):
+class menu_img_sequence(bpy.types.Operator):
     """Creates an image sequence for the duration of the frame range."""
     bl_idname = "wm.ss_seq"
     bl_label = "Image Sequence"
@@ -97,7 +98,7 @@ class screenshotSequence(bpy.types.Operator):
         wm.event_timer_remove(self._timer)
 
 
-class menuLoadstate(bpy.types.Operator):
+class menu_quick_load(bpy.types.Operator):
     """Loads the last state saved."""
     bl_idname = "wm.quick_load"
     bl_label = "Quick Load"
@@ -107,7 +108,7 @@ class menuLoadstate(bpy.types.Operator):
         return {'FINISHED'}
 
 
-class menuSavestate(bpy.types.Operator):
+class menu_quick_save(bpy.types.Operator):
     """Saves to the oldest state."""
     bl_idname = "wm.quick_save"
     bl_label = "Quick Save"
@@ -117,7 +118,151 @@ class menuSavestate(bpy.types.Operator):
         return {'FINISHED'}
 
 
-class menuPreview(bpy.types.Operator):
+class menu_saveslot_1(bpy.types.Operator):
+    """Saves to slot #1"""
+    bl_idname = "wm.save_slot_1"
+    bl_label = "Save Slot 1"
+
+    def execute(self, context):
+        context.scene.my_tool.selected_slot = 1
+        slot = context.scene.my_tool.selected_slot
+        save_slot_state(slot)
+        return {'FINISHED'}
+
+
+class menu_saveslot_2(bpy.types.Operator):
+    """Saves to slot #2"""
+    bl_idname = "wm.save_slot_2"
+    bl_label = "Save Slot 2"
+
+    def execute(self, context):
+        context.scene.my_tool.selected_slot = 2
+        slot = context.scene.my_tool.selected_slot
+        save_slot_state(slot)
+        return {'FINISHED'}
+
+
+class menu_saveslot_3(bpy.types.Operator):
+    """Saves to slot #3"""
+    bl_idname = "wm.save_slot_3"
+    bl_label = "Save Slot 3"
+
+    def execute(self, context):
+        context.scene.my_tool.selected_slot = 3
+        slot = context.scene.my_tool.selected_slot
+        save_slot_state(slot)
+        return {'FINISHED'}
+
+
+class menu_saveslot_4(bpy.types.Operator):
+    """Saves to slot #4"""
+    bl_idname = "wm.save_slot_4"
+    bl_label = "Save Slot 3"
+
+    def execute(self, context):
+        context.scene.my_tool.selected_slot = 4
+        slot = context.scene.my_tool.selected_slot
+        save_slot_state(slot)
+        return {'FINISHED'}
+
+
+class menu_saveslot_5(bpy.types.Operator):
+    """Saves to slot #5"""
+    bl_idname = "wm.save_slot_5"
+    bl_label = "Save Slot 5"
+
+    def execute(self, context):
+        context.scene.my_tool.selected_slot = 5
+        slot = context.scene.my_tool.selected_slot
+        save_slot_state(slot)
+        return {'FINISHED'}
+
+
+class menu_saveslot_6(bpy.types.Operator):
+    """Saves to slot #6"""
+    bl_idname = "wm.save_slot_6"
+    bl_label = "Save Slot 6"
+
+    def execute(self, context):
+        context.scene.my_tool.selected_slot = 6
+        slot = context.scene.my_tool.selected_slot
+        save_slot_state(slot)
+        return {'FINISHED'}
+
+
+class menu_loadslot_1(bpy.types.Operator):
+    """Loads from slot #1"""
+    bl_idname = "wm.load_slot_1"
+    bl_label = "Load Slot 1"
+
+    def execute(self, context):
+        context.scene.my_tool.selected_slot = 1
+        slot = context.scene.my_tool.selected_slot
+        load_slot_state(slot)
+        return {'FINISHED'}
+
+
+class menu_loadslot_2(bpy.types.Operator):
+    """Loads from slot #2"""
+    bl_idname = "wm.load_slot_2"
+    bl_label = "Load Slot 2"
+
+    def execute(self, context):
+        context.scene.my_tool.selected_slot = 2
+        slot = context.scene.my_tool.selected_slot
+        load_slot_state(slot)
+        return {'FINISHED'}
+
+
+class menu_loadslot_3(bpy.types.Operator):
+    """Loads from slot #3"""
+    bl_idname = "wm.load_slot_3"
+    bl_label = "Load Slot 3"
+
+    def execute(self, context):
+        context.scene.my_tool.selected_slot = 3
+        slot = context.scene.my_tool.selected_slot
+        load_slot_state(slot)
+        return {'FINISHED'}
+
+
+class menu_loadslot_4(bpy.types.Operator):
+    """Loads from slot #4"""
+    bl_idname = "wm.load_slot_4"
+    bl_label = "Load Slot 3"
+
+    def execute(self, context):
+        context.scene.my_tool.selected_slot = 4
+        slot = context.scene.my_tool.selected_slot
+        load_slot_state(slot)
+        return {'FINISHED'}
+
+
+class menu_loadslot_5(bpy.types.Operator):
+    """Loads from slot #5"""
+    bl_idname = "wm.load_slot_5"
+    bl_label = "Load Slot 5"
+
+    def execute(self, context):
+        context.scene.my_tool.selected_slot = 5
+        slot = context.scene.my_tool.selected_slot
+        load_slot_state(slot)
+        return {'FINISHED'}
+
+
+class menu_loadslot_6(bpy.types.Operator):
+    """Loads from slot #6"""
+    bl_idname = "wm.load_slot_6"
+    bl_label = "Load Slot 6"
+
+    def execute(self, context):
+        context.scene.my_tool.selected_slot = 6
+        slot = context.scene.my_tool.selected_slot
+        load_slot_state(slot)
+        return {'FINISHED'}
+
+
+class menu_img_preview(bpy.types.Operator):
     """Steps through the current frame range."""
     bl_idname = "wm.prev_seq"
     bl_label = "Preview Sequence"
