759844def645a5d8c472f415777d9fc2289b7644
KELLZ <forestruns@gmail.com>
2023-01-03 15:42:56 -0330

    Added 'GLOBAL_TIMER' offset and CURRENT_FRAME. Added some comments.

--- External-Melee-Camera/emc_common.py
+++ External-Melee-Camera/emc_common.py
@@ -1,24 +1,12 @@
 import bpy
 import pymem
-from pymem.ptypes import RemotePointer
 
 pm = pymem.Pymem("Slippi Dolphin.exe")
-'''
-INDEX_PTR = 0x01CF0088
 
-
-def get_ptr(base, offsets):
-    remote_pointer = RemotePointer(pm.process_handle, base)
-    for offset in offsets:
-        if offset != offsets[-1]:
-            remote_pointer = RemotePointer(pm.process_handle, remote_pointer.value + offset)
-        else:
-            return remote_pointer.value + offset
-'''
-
-#FRAME_INDEX = get_ptr(pm.base_address + INDEX_PTR, [0xC])
+# Page size to look for in pattern_scan_all, as there could be multiple pages with GALE01.
 EMU_SIZE = 0x2000000
 EMU_DIST = 0x10000
+# Dolphin specific patterns of their respective functions.
 SAVE_STATE_PAT = b'...\x5C\x24\x10\x55\x48\x8B\xEC\x48\x83\xEC\x50\x48\x8D\x4D\xE0'
 LOAD_STATE_PAT = b'...\x5C\x24\x08\x57\x48\x83\xEC\x70\x48\x8B\x05....\x48\x33\xC4\x48\x89\x44.\x60\x8B\xD9\x48\x8D\x4C.\x30\xE8....'
 SCREEN_SHOT_PAT = b'\x40\x53\x48\x83\xEC\x50\x48\x8B\x05....\x48\x33\xC4\x48\x89\x44.\x40\x48\x8B\x05....'
@@ -26,6 +14,9 @@ FRAME_STEP_PAT = b'..\x5C\x24\x08\x57\x48\x83\xEC\x20\x48\x8B\xF9\xE8....\x83\xF
 SAVE_STATE_SLOT_PAT = b'\x48\x89...\x48\x89...\x57\x48\x83\xEC\x60\x48\x8B..\x98\xB3\x00'
 LOAD_STATE_SLOT_PAT = b'..\x5C\x24\x10\x57\x48\x83\xEC\x60\x48\x8B\x05\x47\xAF\xB3\x00'
 # FREE_LOOK = None
+
+# Melee specific offsets. These will always be found x distance from GALE01 (start of emu)
+GLOBAL_TIMER = 0x479D60
 CAM_START = 0x453040
 PAUSE_BIT = 0x479D68
 CAM_TYPE = 0x452C6F
@@ -77,6 +68,8 @@ def find_dolphin_funcs(byte_pattern):
     found = pymem.pattern.pattern_scan_module(handle, module, byte_pattern, return_multiple=False)
     return found
 
+# Setting variables of functions by finding their byte pattern in the main Dolphin thread.
+# Subtracting base address as 'call_native_func()' will add it. Trying to stick to the format of using base + offset.
 SAVE_STATE = find_dolphin_funcs(SAVE_STATE_PAT) - pm.base_address
 LOAD_STATE = find_dolphin_funcs(LOAD_STATE_PAT) - pm.base_address
 SCREEN_SHOT = find_dolphin_funcs(SCREEN_SHOT_PAT) - pm.base_address
@@ -84,4 +77,4 @@ FRAME_STEP = find_dolphin_funcs(FRAME_STEP_PAT) - pm.base_address
 SAVE_STATE_SLOT = find_dolphin_funcs(SAVE_STATE_SLOT_PAT) - pm.base_address
 LOAD_STATE_SLOT = find_dolphin_funcs(LOAD_STATE_SLOT_PAT) - pm.base_address
 GALE01 = find_gale01()
-
+CURRENT_FRAME = GALE01 + GLOBAL_TIMER
